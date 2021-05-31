f0a135cd419f816da1b349852bb72a9953e1bff7
Trung Nguyen <thanhtrung1012000@gmail.com>
2021-06-01 03:02:51 +0700

    css and notfound setup

---
+++ client/src/components/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div className='relative flex justify-center' style={{minHeight: 'calc(100vh - 70px)'}}>
+            <h2 className='absolute text-3xl font-semibold text-gray-500 top-1/2'>404 | Not Found.</h2>
+        </div>
+    )
+}
+
+export default NotFound
---
+++ server.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+    res.json({ msg: 'Hello' });
+});
+
+const URI = process.env.MONGODB_URL;
+mongoose.connect(URI, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, err => {
+    if (err) throw err;
+    console.log('Connect to mongoDb');
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log('Server is running on port ', port);
+});
